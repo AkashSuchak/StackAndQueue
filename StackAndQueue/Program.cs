1d47c58d6a786986d81d12da530887df01315737
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-14 01:28:51 +0530

    feat: Added: Creat Queue, Add Data and Display

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -10,7 +10,7 @@ namespace StackAndQueue
             Console.WriteLine("Welcome to Stack and Queue !");
             Console.WriteLine("----------------------------");
 
-            //Calling Class and Adding Data in Stack
+            /*//Calling Class and Adding Data in Stack
             LinkedListStack linkedListStack = new LinkedListStack();
             linkedListStack.Push(70);
             linkedListStack.Push(30);
@@ -24,7 +24,16 @@ namespace StackAndQueue
 
             //Pop value till Stack will empty
             linkedListStack.IsEmpty();
+*/
+            //Calling Class and Adding Data in Stack
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("----------------");
 
+            //Display Queue
+            linkedListQueue.Display();
         }
     }
 }

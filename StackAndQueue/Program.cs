b092f0d13c61a20d93250fbabecea292020de092
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-13 18:46:59 +0530

    feat: Welcome Message

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue !");
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
